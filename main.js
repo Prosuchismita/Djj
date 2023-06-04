4e79dd198a3856d8112b07d525be594cf72d8a19
Suchismita Mukherjee <89072568+Prosuchismita@users.noreply.github.com>
2023-06-04 17:38:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+peter_pan_song = "";
+Harry_potter_theme_song = "";
+leftWrist_x = 0;
+leftWrist_y = 0;
+rightWrist_x = 0;
+rightWrist_y = 0;
+
+
+function setup(){
+    canvas= createCanvas(600,530);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+
+}
+
+function draw(){
+    image(video,0,0,600,530);
+}
+
+function preload(){
+    peter_pan_song = loadSound("music.mp3");
+    Harry_potter_theme_song = loadSound("music2.mp3");
+}
+
+function modelLoaded(){
+    console.log("PoseNet Is initialized");
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+
+        leftWrist_x = results[0].pose.leftWrist.x;
+        leftWrist_y = results[0].pose.leftWrist.y;
+        console.log("leftWrist_x = "+leftWrist_x+"leftWrist_y = "+leftWrist_y);
+
+        rightWrist_x = results[0].pose.rightWrist.x;
+        rightWrist_y = results[0].pose.rightWrist.y;
+        console.log("rightWrist_x = "+rightWrist_x+"rightWrist_y = "+righttWrist_y);
+    }
+}
